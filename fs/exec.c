d6d9f921e058f63d908ebb0ff997ff65eb9d715e
bheatleyyy <sugakesshaa@gmail.com>
2022-03-07 21:51:38 +0530

    [TEMP] fs: exec: Correct hwcomposer prefix path

--- fs/exec.c
+++ fs/exec.c
@@ -78,7 +78,7 @@ int suid_dumpable = 0;
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
-#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
+#define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware.display.composer-service"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
