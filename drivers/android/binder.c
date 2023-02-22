c62572952b7bd54fa9627f6ada092c8416ac067d
Cyber Knight <cyberknight755@gmail.com>
2023-02-22 06:09:36 +0000

    binder: Stub out remnants of freezer ioctls
    - This fixes the following error:
    ../drivers/android/binder.c:4906:12: error: unused function 'binder_ioctl_freeze' [-Werror,-Wunused-function]
    static int binder_ioctl_freeze(struct binder_freeze_info *info,
               ^
    ../drivers/android/binder.c:4954:12: error: unused function 'binder_ioctl_get_freezer_info' [-Werror,-Wunused-function]
    static int binder_ioctl_get_freezer_info(
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4884,7 +4884,7 @@ static int binder_ioctl_get_node_debug_info(struct binder_proc *proc,
 
 	return 0;
 }
-
+#if 0
 static bool binder_txns_pending_ilocked(struct binder_proc *proc)
 {
 	struct rb_node *n;
@@ -4978,6 +4978,7 @@ static int binder_ioctl_get_freezer_info(
 
 	return 0;
 }
+#endif
 
 static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
